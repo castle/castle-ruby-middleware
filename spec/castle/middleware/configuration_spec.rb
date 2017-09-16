ab9675d8df38564a5bae20ab648e0c1895517ef8
Sebastian Wallin <sebastian.wallin@gmail.com>
2017-09-16 15:10:43 -0700

    Added test for Regexp support

--- spec/castle/middleware/configuration_spec.rb
+++ spec/castle/middleware/configuration_spec.rb
@@ -33,4 +33,22 @@ describe Castle::Middleware::Configuration do
       it { is_expected.to be_eql 0 }
     end
   end
+
+  describe '#events.path' do
+    subject { instance.events.values.first['path'] }
+
+    before do
+      allow(instance).to receive(:file_path) do
+        File.expand_path(path, File.dirname(__FILE__))
+      end
+
+      instance.load_config_file!
+    end
+
+    context 'when path config contains regex' do
+      let(:path) { './castle_config.yml' }
+
+      it { is_expected.to be_an_instance_of(::Regexp) }
+    end
+  end
 end
