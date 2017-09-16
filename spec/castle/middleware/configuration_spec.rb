2bee0b7f0dd20b3312b23e6ec2d1090c2fd3f2c2
Sebastian Wallin <sebastian.wallin@gmail.com>
2017-09-16 14:33:35 -0700

    Configurable path to config YML

--- spec/castle/middleware/configuration_spec.rb
+++ spec/castle/middleware/configuration_spec.rb
@@ -1,13 +1,37 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'pp'
 
 describe Castle::Middleware::Configuration do
+  subject(:instance) { described_class.new }
   describe '#reset!' do
-    subject(:instance) { described_class.new }
-
     before { instance.api_secret = 'secret' }
 
     it { expect { instance.reset! }.to change(instance, :api_secret).to nil }
+  end
+
+  describe '#events.size' do
+    subject { instance.events.size }
+
+    before do
+      allow(instance).to receive(:file_path) do
+        File.expand_path(path, File.dirname(__FILE__))
+      end
+
+      instance.load_config_file!
+    end
+
+    context 'when existing config file is loaded' do
+      let(:path) { './castle_config.yml' }
+
+      it { is_expected.to be_eql 1 }
+    end
+
+    context 'when non-existing config is loaded' do
+      let(:path) { './non-existing.yml' }
+
+      it { is_expected.to be_eql 0}
+    end
   end
 end
