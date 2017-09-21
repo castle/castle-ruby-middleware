3968a5e5382296768e895a8f7ce51741021bd04d
Sebastian Wallin <sebastian.wallin@gmail.com>
2017-09-20 21:42:15 -0700

    More tests for Sensor

--- spec/castle/middleware/sensor_spec.rb
+++ spec/castle/middleware/sensor_spec.rb
@@ -7,6 +7,8 @@ describe Castle::Middleware::Sensor do
   let(:env) { {} }
   let(:response) { [status, headers, body] }
   let(:request_config) { ::Castle::Middleware::RequestConfig.new }
+  let(:body) { ['<html><head></head></html>'] }
+  let(:headers) { { 'Content-Type' => 'text/html' } }
 
   before do
     # Fake Rack Response
@@ -22,6 +24,7 @@ describe Castle::Middleware::Sensor do
       end
     end
 
+    allow(Castle::Middleware.configuration).to receive(:api_secret) { 'secret' }
     allow(env).to receive(:[]).with(described_class::JS_IS_INJECTED_KEY) { false }
     allow(env).to receive(:[]).with('castle') { request_config }
     allow(app).to receive(:call).and_return(response)
@@ -33,13 +36,23 @@ describe Castle::Middleware::Sensor do
     end
   end
 
+  matcher :inject_the_identify_tag do
+    match_unless_raises do |subscriber|
+      expect(subscriber[2]).to include "_castle('identify',"
+    end
+  end
+
+  matcher :inject_the_secure_tag do
+    match_unless_raises do |subscriber|
+      expect(subscriber[2]).to include "_castle('secure',"
+    end
+  end
+
   describe '#call' do
     subject { described_class.new(app).call(env) }
 
     context 'with HTML body' do
       let(:status) { 200 }
-      let(:body) { ['<html><head></head></html>'] }
-      let(:headers) { { 'Content-Type' => 'text/html' } }
 
       it { is_expected.to inject_the_script }
     end
@@ -47,9 +60,17 @@ describe Castle::Middleware::Sensor do
     context 'with non 200 code' do
       let(:status) { 400 }
       let(:body) { [''] }
-      let(:headers) { { 'Content-Type' => 'text/html' } }
 
       it { is_expected.to_not inject_the_script }
+    end
+
+    context 'when user_id is set' do
+      let(:status) { 200 }
+
+      before { request_config.identify(1, nil) }
+
+      it { is_expected.to inject_the_identify_tag }
+      it { is_expected.to inject_the_secure_tag }
     end
   end
 end
